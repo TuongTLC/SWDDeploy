ad60d92aed44d1dd7f4db0bcac3aeb05859f7b8f
TuongTLC <53476330+TuongTLC@users.noreply.github.com>
2022-03-09 17:41:51 +0700

    asdd

---
+++ src/main/java/com/example/testdeployswd/TestDeploySwdApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdeployswd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeploySwdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDeploySwdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testdeployswd/TestDeploySwdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdeployswd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeploySwdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
